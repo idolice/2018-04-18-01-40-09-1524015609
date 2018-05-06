eb15f491e833e9ec75a53e6162f5c4b3fef90a3a
bingwang <bingwang@thoughtworks.com>
2018-05-06 11:24:20 +0800

    [Bing] add one more test

--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -1,9 +1,6 @@
 package com.example.employee;
 
-import com.example.employee.entity.Company;
-import com.example.employee.entity.Employee;
 import com.example.employee.repository.EmployeeRepository;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,18 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @RunWith(SpringRunner.class)
@@ -64,7 +51,7 @@ public class EmployeeJPATest {
 
     @Test
     public void should_find_employees_salary_between_6000_to_7000() {
-        //找到工资在6000-7000范围内的employee
+        //3.找到工资在6000-7000范围内的employee
 //        int employeeSize = 3;
 //        List<Employee> employees = null;
 //        AssertionsForClassTypes.assertThat(employees.size()).isEqualTo(employeeSize);
@@ -73,7 +60,7 @@ public class EmployeeJPATest {
 
     @Test
     public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() throws Exception {
-        //3.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
+        //4.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
 //        Employee expectedEmployee = new Employee(1, "xiaohong",19,"female",7000,1);
 //        Employee employee = null;
 //        String actualName = employee.getName();
@@ -83,16 +70,25 @@ public class EmployeeJPATest {
 
     @Test
     public void should_return_employee_list_when_input_page_request() throws Exception {
-        //4.实现对Employee的分页查询，每页两条数据，一共三页数。
+        //5.实现对Employee的分页查询，每页两条数据，一共三页数。
 //        assertThat(EmployeePage.getTotalPages()).isEqualTo(3);
     }
 
     @Test
     public void should_return_company_name_when_input_employee_name() throws Exception {
-        //5.查找xiaohong的所在的公司的公司名称
+        //6.查找xiaohong的所在的公司的公司名称
 //        String expectedCompanyName = "alibaba";
 //        Company company = null;
 //        String actualCompanyName = company.getCompanyName();
 //        assertThat(actualCompanyName).isEqualTo(expectedCompanyName);
     }
+
+    @Test
+    public void should_find_employee_by_query() {
+        //6.通过写JPQL查询名字含有"xiaozhi"的employee
+//        String expectedName = "xiaozhi";
+//        Employee employee = null;
+//        assertThat(expectedName).isEqualTo(employee.getName());
+    }
+
 }
